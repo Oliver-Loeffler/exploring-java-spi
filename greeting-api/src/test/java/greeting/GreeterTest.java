b0a5fc8673106c2018ae7b3a423b7f40959d56ce
Oliver Loeffler <oliver@raumzeitfalle.de>
2019-05-25 11:36:25 +0200

    API sub-project added.

---
+++ greeting-api/src/test/java/greeting/GreeterTest.java
@@ -0,0 +1,37 @@
+package greeting;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class GreeterTest {
+
+	private Greeter classUnderTest;
+	
+	@BeforeEach
+	public void prepare() {
+		this.classUnderTest = createGreeter("HelloDude");
+	}
+	
+	@Test
+	public void getSalutation() {
+		assertEquals("HelloDude", classUnderTest.getSalutation());
+	}
+	
+	private Greeter createGreeter(String salutation) {
+		Greeter greeter = new Greeter() {
+
+			@Override
+			public String getSalutation() {
+				return salutation;
+			}
+			
+		};
+		return greeter;
+	}
+
+
+
+}
